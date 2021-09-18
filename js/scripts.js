78fa5b9c5a482e898cf81d2a2b75efe8209bbc59
Anikal25 <kal21006@byui.edu>
2021-09-18 17:48:25 +0100

    week 1 portal assignment

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,5 @@ let myDate = new Date();
 let myYear = myDate.getFullYear();
 document.getElementById("theYear").textContent = myYear;
 
+
 document.getElementById("lu").textContent = "Last Updated: " + document.lastModified;
