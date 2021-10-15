86a641065e652f224f6380955eb2c517aa7953bf
Anikal25 <kal21006@byui.edu>
2021-10-15 21:25:40 +0100

    Lesson 5

---
+++ lesson5/js/date.js
@@ -0,0 +1,28 @@
+const date = new Date();
+
+const options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+
+document.getElementById('currentdate');
+currentdate.textContent = date.toLocaleDateString('en-GB', options);
+
+document.getElementById('theYear');
+theYear.textContent = date.getFullYear();
+
+
+function bannerCalculator() {
+  const today = d.getDay();
+  const pancake = document.querySelector(".pancake");
+  
+  if (today = 15) {
+      pancake.style.display = 'block';
+  }
+  
+  else {
+      pancake.style.display = 'none';
+  }
+}
