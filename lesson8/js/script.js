617687db9509f22c526cf042e01fc210067ea24e
Anikal25 <kal21006@byui.edu>
2021-11-04 23:20:57 +0100

    Storm Center Page

---
+++ lesson8/js/script.js
@@ -0,0 +1,23 @@
+function toggleMenu() {
+    console.log(document.getElementById("bid").classList);
+    document.getElementById("bid").classList.toggle("hide");
+}
+
+
+
+
+
+
+
+//let myDate = new Date();
+//let myYear = myDate.getFullYear();
+//document.getElementById("theYear").textContent = myYear;
+
+
+//document.getElementById("lu").textContent = "Last Updated: " + document.lastModified;
+
+//let today = new Date();
+//let str = today.toGMTString();
+
+//document.getElementById("lu").textContent = str;
+
---
+++ lesson8/js/visit.js
@@ -0,0 +1,22 @@
+const millisecondsToDays = 86400000;
+
+let lastVisit = localStorage.getItem("lastvisit");
+
+console.log(lastVisit);
+
+console.log(Date.now())
+
+let todaysVisit = Math.floor(Date.now() / millisecondsToDays);
+
+let daysFromVisit = todaysVisit - lastVisit;
+
+localStorage.setItem("lastvisit", todaysVisit);
+
+console.log(todaysVisit);
+
+if (lastVisit != null) {
+
+    document.getElementById("visit").textContent = daysFromVisit;
+} else {
+    document.getElementById("visit").textContent = 0;
+}
