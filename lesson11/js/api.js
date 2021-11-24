f71708c808f81b51e05a159f6db067ef9a789902
Anikal25 <kal21006@byui.edu>
2021-11-24 16:26:03 +0100

    The Weather Site

---
+++ lesson11/js/api.js
@@ -0,0 +1,17 @@
+const apiUrl = "//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=14d6f75c6393671b4385e8e50078b55b&units=imperial";
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+ 
+
+document.getElementById('current-temp').textContent = jsObject.list[0].main.temp;
+
+const imagesrc = `https://openweathermap.org/img/w/${jsObject.list[0].weather[0].icon}.png`;  // note the concatenation
+const desc = jsObject.list[0].weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+
+});
